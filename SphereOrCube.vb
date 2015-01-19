75c7efa0f15e94f5561e60a8a56320f18d33affa
Argus Beane <beaneargus@gmail.com>
2015-01-19 15:20:17 -0600

    Ready to turn in
    Added try/catch and while around input for sphere/cube
    Added try/catch around inputs and calculations

--- SphereOrCube.vb
+++ SphereOrCube.vb
@@ -1,35 +1,57 @@
 ﻿Module Module1
     Sub Main()
         ' Set Variables
-        Dim input As String
+        Dim input As Integer
         Dim Volume As Double = 0
         Dim Length As Double = 0
         Dim Radius As Double = 0
         Dim ResultString As String = ""
 
-        ' Cube or Sphere
-        Console.WriteLine("Calculating Volume: input 1 for cube,input 2 for sphere")
-        input = Console.ReadLine()
+        While input < 1
+            Try
+                ' Cube or Sphere
+                Console.WriteLine("Calculating Volume: input 1 for cube,input 2 for sphere")
+                input = Console.ReadLine()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End While
+
 
         If input = "1" Then
             ' - Cube
 
-            ' --Prompt for Length of Side
-            Console.WriteLine("Input length of side")
-            Length = Console.ReadLine()
-
             ' --Calculate Cube Volume
-            Volume = Length ^ 3
+            Try
+                ' --Prompt for Length of Side
+                Console.WriteLine("Input length of side")
+                Length = Console.ReadLine()
+                Volume = Length ^ 3
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+                Exit Sub
+            Finally
+                Console.WriteLine("Calculation of cube completed correctly.")
+            End Try
+
 
         ElseIf input = "2" Then
             ' - Sphere
 
-            ' -- Prompt for Radius
-            Console.WriteLine("Input sphere radius")
-            Radius = Console.ReadLine()
-
             ' -- Calculate Sphere Volume
-            Volume = (4 / 3) * Math.PI * (Radius ^ 3)
+            Try
+                ' -- Prompt for Radius
+                Console.WriteLine("Input sphere radius")
+                Radius = Console.ReadLine()
+                Volume = (4 / 3) * Math.PI * (Radius ^ 3)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+                Exit Sub
+            Finally
+                Console.WriteLine("Calculation of sphere completed correctly.")
+            End Try
+
         End If
 
         ' Output Volume
